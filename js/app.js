45f2fadc850479efb8d6ebc425ea7a38d79bf0a6
Andre <andre_5991@yahoo.com>
2014-11-03 12:12:34 -0800

    in class example

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@
 
 "use strict";
 
+//create new angular module
+//chaining stuff together put semicolon after chain ends
+//parameter for controller always have to be $scope, creates a scope object
+//hands it in to the angular object, what the views will be able to see
+angular.module('MovieApp', [])
+    .controller('MoviesController', function($scope){
+        $scope.movies = movies;
+        $scope.searchString = '';
+        $scope.sortCol = 'rank';
+        $scope.sortReverse = false;
+
+        $scope.sortBy = function (colName) {
+            if ($scope.sortCol == colName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            }
+            else {
+                $scope.sortReverse = false;
+            }
+            $scope.sortCol = colName;
+        }
+    });
